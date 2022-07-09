7938faca1934e92ce084788d6fa28a10e078625c
TeodoroWillian <totawillian@gmail.com>
2022-07-09 19:28:36 -0400

    PUT, POST e DELETE

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const cors = require('cors');
-const routes = require('./src/routes/paleta.route');
+const cors = require("cors");
+const routes = require("./src/routes/paleta.route");
 
 const port = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/paletas',routes);
+app.use("/paletas", routes);
 
 app.listen(port, () => {
   console.log(`servidor rodando em http://localhost:${port}`);
