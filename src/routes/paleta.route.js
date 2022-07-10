1ed081376d68b4e43ba82171556f6816c5fc2759
TeodoroWillian <totawillian@gmail.com>
2022-07-10 16:10:17 -0400

    config swagger

--- src/routes/paleta.route.js
+++ src/routes/paleta.route.js
@@ -1,10 +1,36 @@
 const route = require("express").Router();
 const controllerPaletas = require("../controllers/paleta.controller");
-const { validId,validObjectBody } = require("../middlewares/paleta.middleware");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../../swagger.json");
+
+const {
+  validId,
+  validObjectBody,
+} = require("../middlewares/paleta.middleware");
+
+route.use("/api-docs", swaggerUi.serve);
+route.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 route.get("/all-paletas", controllerPaletas.findAllPaletasController);
-route.get("/one-paleta/:id",validId, controllerPaletas.findByIdPaletaController);
-route.post("/create-paleta",validObjectBody, controllerPaletas.createPaletaController);
-route.put("/update-paleta/:id",validId,validObjectBody, controllerPaletas.updatePaletaController);
-route.delete("/delete-paleta/:id",validId, controllerPaletas.deletePaletaController);
+route.get(
+  "/one-paleta/:id",
+  validId,
+  controllerPaletas.findByIdPaletaController
+);
+route.post(
+  "/create-paleta",
+  validObjectBody,
+  controllerPaletas.createPaletaController
+);
+route.put(
+  "/update-paleta/:id",
+  validId,
+  validObjectBody,
+  controllerPaletas.updatePaletaController
+);
+route.delete(
+  "/delete-paleta/:id",
+  validId,
+  controllerPaletas.deletePaletaController
+);
 module.exports = route;
